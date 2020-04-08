de264b5fc055768e2546b4a1f109a47105081732
John Daly <jdaly@panix.com>
2020-04-08 14:42:08 -0400

    updated to AndroidX

--- app/src/main/java/gov/townofsouthamptonny/android/youthservices/YouthServicesFragment.java
+++ app/src/main/java/gov/townofsouthamptonny/android/youthservices/YouthServicesFragment.java
@@ -3,7 +3,7 @@ package gov.townofsouthamptonny.android.youthservices;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
--- app/src/main/java/gov/townofsouthamptonny/android/youthservices/YouthServicesPagerActivity.java
+++ app/src/main/java/gov/townofsouthamptonny/android/youthservices/YouthServicesPagerActivity.java
@@ -3,11 +3,11 @@ package gov.townofsouthamptonny.android.youthservices;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
